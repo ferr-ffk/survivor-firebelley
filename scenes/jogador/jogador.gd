868372e5c57b07421457d165b5810ea423183f96
Fernando Freitas <freitax.li@gmail.com>
2024-02-28 19:28:47 -0300

    Criação de sprite de jogador

--- scenes/jogador/jogador.gd
+++ scenes/jogador/jogador.gd
@@ -8,15 +8,15 @@ func _ready() -> void:
 func _process(delta) -> void:
 	var direcao: Vector2 = get_vetor_movimento().normalized()
 	
-	velocity += direcao * VELOCIDADE_MAXIMA
+	velocity = direcao * VELOCIDADE_MAXIMA
 	
 	move_and_slide()
 
 func get_vetor_movimento() -> Vector2:
 	var vetor_movimento = Vector2.ZERO
 	
-	var movimento_x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-	var movimento_y = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
+	var movimento_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var movimento_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	return Vector2(movimento_x, movimento_y)
 	
