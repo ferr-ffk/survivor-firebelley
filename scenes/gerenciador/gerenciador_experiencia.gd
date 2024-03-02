6c7ccb9e8d32fda4ea36ee3af77f1605979d6f6b
Fernando Freitas <freitax.li@gmail.com>
2024-03-02 17:21:06 -0300

    Alteração da lógica de ganho de experiência

--- scenes/gerenciador/gerenciador_experiencia.gd
+++ scenes/gerenciador/gerenciador_experiencia.gd
@@ -1,6 +1,13 @@
 extends Node
+class_name GerenciadorExperiencia
+
+signal experiencia_atualizada(experiencia_atual: float, experiencia_maxima: float)
+
+const AUMENTO_DE_EXPERIENCIA_POR_NIVEL = 10
 
 var experiencia_atual = 0.0
+var nivel_atual = 1
+var experiencia_maxima = 5
 
 func _ready() -> void:
 	# conecta ao sinal global de frasco experiencia coletada
@@ -8,8 +15,17 @@ func _ready() -> void:
 
 
 func aumentar_experiencia(n: float) -> void:
-	experiencia_atual += n
-
+	experiencia_atual = min(nivel_atual + n, experiencia_maxima)
+	experiencia_atualizada.emit(experiencia_atual, experiencia_maxima)
+	
+	# a cada nivel alcançado, aumenta o número atual do nivel, e o número de experiencia
+	# necessaria para subir de nivel
+	if experiencia_atual == experiencia_maxima:
+		nivel_atual += 1
+		experiencia_maxima += AUMENTO_DE_EXPERIENCIA_POR_NIVEL
+		experiencia_atual = 0
+		
+		experiencia_atualizada.emit(experiencia_atual, experiencia_maxima)
 
 func on_frasco_experiencia_coletado(n: float) -> void:
 	aumentar_experiencia(n)
---
+++ scenes/ui/barra_experiencia.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@export var gerenciador_experiencia: GerenciadorExperiencia
+
+@onready var barra_vida: ProgressBar = $MarginContainer/BarraVida
+
+func _ready() -> void:
+	barra_vida.value = 0
+	barra_vida.max_value = gerenciador_experiencia.experiencia_maxima
+
+	gerenciador_experiencia.experiencia_atualizada.connect(on_experiencia_atualizada)	
+
+func on_experiencia_atualizada(experiencia_atual: float, experiencia_maxima: float) -> void:
+	var porcentagem = experiencia_atual / experiencia_maxima
+	barra_vida.value = porcentagem 
