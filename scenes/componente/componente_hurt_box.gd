e47d74662683ea5572f7b1205ffe077d83f3ba1b
Fernando Freitas <freitax.li@gmail.com>
2024-03-27 20:00:15 -0300

    Adicionar formatação de dano

--- scenes/componente/componente_hurt_box.gd
+++ scenes/componente/componente_hurt_box.gd
@@ -23,6 +23,8 @@ func _on_area_entered(area: Area2D) -> void:
 	
 	get_tree().get_first_node_in_group("primeiro_plano").add_child(texto_flutuante)
 	
+	var format_string = "%0.1f"
+	
 	texto_flutuante.global_position = global_position + (Vector2.UP * 16)
-	texto_flutuante.inicio(str(componente_hit_box.dano))
+	texto_flutuante.inicio(format_string % componente_hit_box.dano)
 	
