a6374956ca8741e145e15e96e4e1546b0bbed40d
Fernando Freitas <freitax.li@gmail.com>
2024-04-07 19:32:23 -0300

    feat: Criação de diretório para arquivos de áudio

--- scenes/objeto/inimigo_basico/inimigo_basico.gd
+++ scenes/objeto/inimigo_basico/inimigo_basico.gd
@@ -6,6 +6,7 @@ const VELOCIDADE_MAXIMA = 50
 @onready var componente_vida: ComponenteVida = $ComponenteVida
 @onready var componente_velocidade: ComponenteVelocidade = $ComponenteVelocidade
 @onready var visual: Node2D = $Visual
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @export_range(0, 100) var dano: int = 2
 
@@ -18,4 +19,6 @@ func _process(delta: float) -> void:
 	
 	visual.scale = Vector2(direcao_movimento, 1)
 	
-	
+
+func _on_componente_hurt_box_atingido() -> void:
+	audio_stream_player_2d.play()
