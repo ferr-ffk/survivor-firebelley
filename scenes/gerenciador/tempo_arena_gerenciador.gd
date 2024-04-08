6dab8be9c40f8b6cf63665860240f37294f1fc96
Fernando Freitas <freitax.li@gmail.com>
2024-04-08 10:55:09 -0300

    fix: Chamada de método para definir vitória

--- scenes/gerenciador/tempo_arena_gerenciador.gd
+++ scenes/gerenciador/tempo_arena_gerenciador.gd
@@ -32,6 +32,7 @@ func get_tempo_restante() -> float:
 
 
 func _on_timer_timeout() -> void:
-	var tela_vitoria = cena_tela_fim.instantiate()
+	var tela_vitoria = cena_tela_fim.instantiate() as TelaFim
 	
 	owner.add_child(tela_vitoria)
+	tela_vitoria.set_vitoria()
