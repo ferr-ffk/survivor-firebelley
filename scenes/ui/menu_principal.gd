e4e7d984a8367f5c54343ccda266957cd7a96d19
Fernando Freitas <freitax.li@gmail.com>
2024-04-11 10:48:32 -0300

    feat: Criação de menu de opções

--- scenes/ui/menu_principal.gd
+++ scenes/ui/menu_principal.gd
@@ -1,9 +1,24 @@
 extends CanvasLayer
 class_name MenuPrincipal
 
+var menu_opcoes = preload("res://scenes/ui/menu_opcoes.tscn")
+
 
 func _on_botao_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/principal/principal.tscn")
+
+
+func _on_botao_opcoes_pressed() -> void:
+	var instancia_menu_opcoes = menu_opcoes.instantiate() as MenuOpcoes
+	
+	add_child(instancia_menu_opcoes)
+	
+	instancia_menu_opcoes.voltar_pressionado.connect(_on_botao_voltar_pressionado.bind(instancia_menu_opcoes))
+
+
+func _on_botao_voltar_pressionado(instancia_menu_opcoes: Node) -> void:
+	instancia_menu_opcoes.queue_free()
+	
 
 
 func _on_botao_sair_pressed() -> void:
