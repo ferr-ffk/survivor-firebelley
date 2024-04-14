0763e549c0339d599317022931650ed3983215f7
Fernando Freitas <freitax.li@gmail.com>
2024-04-14 11:10:57 -0300

    feat: Commit inicial

---
+++ scenes/ui/menu_opcoes.gd
@@ -0,0 +1,67 @@
+extends CanvasLayer
+class_name MenuOpcoes
+
+## volta para a cena anterior
+signal voltar_pressionado
+
+@onready var slider_sfx: HSlider = %SliderSFX
+@onready var slider_musica: HSlider = %SliderMusica
+@onready var check_box_tela_cheia: CheckBox = %CheckBoxTelaCheia
+@onready var botao_voltar: Button = %BotaoVoltar
+@onready var label_tela_cheia: Label = %LabelTelaCheia
+
+
+func _ready() -> void:
+	slider_sfx.value = get_volume_bus_porcentagem("sfx")
+	slider_musica.value = get_volume_bus_porcentagem("musica")
+	
+	# para reutilizar os métodos, cria-se uma função que altera qualquer slider, passando o nome
+	slider_sfx.value_changed.connect(_on_slider_audio_changed.bind("sfx"))
+	slider_musica.value_changed.connect(_on_slider_audio_changed.bind("musica"))
+	
+	
+	if DisplayServer.window_get_mode() == Window.MODE_FULLSCREEN:
+		check_box_tela_cheia.toggle_mode = false
+	else:
+		check_box_tela_cheia.toggle_mode = true
+
+
+func get_volume_bus_porcentagem(bus_name: String) -> float:
+	# obtem o indice do bus para se alterar
+	var bus_index: int = AudioServer.get_bus_index(bus_name)
+
+	# obtem o volume em decibeis do bus	
+	var volume_decibel: float = AudioServer.get_bus_volume_db(bus_index)
+	
+	# converte e retorna os decibeis para uso em porcentagem do slider
+	return db_to_linear(volume_decibel)
+
+
+func set_volume_bus_porcentagem(bus_name: String, porcentagem: float) -> void:
+	# obtem o indice do bus para se alterar
+	var bus_index: int = AudioServer.get_bus_index(bus_name)
+	
+	# converte porcentagem em decibeis
+	var volume_decibel: float = linear_to_db(porcentagem)
+	
+	# altera o valor no audio
+	AudioServer.set_bus_volume_db(bus_index, volume_decibel)
+
+
+func _on_slider_audio_changed(value: float, bus_name: String):
+	set_volume_bus_porcentagem(bus_name, value)
+
+
+func _on_check_box_tela_cheia_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
+		label_tela_cheia.text = "Modo Janela"
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+		label_tela_cheia.text = "Modo Tela Cheia"
+
+
+func _on_botao_voltar_pressed() -> void:
+	voltar_pressionado.emit()
---
+++ scenes/ui/menu_pausa.gd
@@ -0,0 +1,79 @@
+extends CanvasLayer
+class_name MenuPausa
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var container_botoes: PanelContainer = %ContainerBotoes
+
+var cena_menu_opcoes = preload("res://scenes/ui/menu_opcoes.tscn")
+
+var fechando_cena: bool
+
+func _ready() -> void:
+	get_tree().paused = true
+	
+	animation_player.play("padrao")	
+	
+	var tween = get_tree().create_tween()
+	
+	# garante que o container terá uma escala de zero ao iniciar a animação
+	tween.tween_property(container_botoes, "scale", Vector2.ZERO, 0)
+	
+	tween.tween_property(container_botoes, "scale", Vector2.ONE, 0.3)\
+		.set_ease(Tween.EASE_OUT)\
+		.set_trans(Tween.TRANS_BACK)
+		
+		
+func fechar_cena() -> void:
+	if fechando_cena:
+		return
+	
+	# para que a animação não seja interrompida no meio por vários inputs seguidos
+	fechando_cena = true
+	
+	animation_player.play_backwards("padrao")
+	
+	var tween = create_tween()
+	
+	container_botoes.pivot_offset = container_botoes.size / 2
+	
+	# garante que o container terá uma escala de zero ao iniciar a animação
+	tween.tween_property(container_botoes, "scale", Vector2.ONE, 0)
+	
+	tween.tween_property(container_botoes, "scale", Vector2.ZERO, 0.3)\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_BACK)
+		
+	# aguarda o termino da animação para deletar a cena atual e voltar para o jogo
+	await tween.finished
+	get_tree().paused = false
+	queue_free()
+		
+		
+## para inputs mapeados que não foram manipulados por nenhum node
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pausa"):
+		fechar_cena()
+		
+		# define o input como manipulado, para que nenhum outro node consiga alterar
+		get_tree().root.set_input_as_handled()
+
+
+func _on_voltar_pressionado(menu_opcoes: Node) -> void:
+	menu_opcoes.queue_free()
+
+
+func _on_botao_retomar_pressed() -> void:
+	fechar_cena()
+
+
+func _on_botao_opcoes_pressed() -> void:
+	var menu_opcoes = cena_menu_opcoes.instantiate() as MenuOpcoes
+	
+	add_child(menu_opcoes)
+	menu_opcoes.voltar_pressionado.connect(_on_voltar_pressionado.bind(menu_opcoes))
+
+
+func _on_botao_sair_menu_pressed() -> void:
+	get_tree().paused = false
+	
+	get_tree().change_scene_to_file("res://scenes/ui/menu_principal.tscn")
