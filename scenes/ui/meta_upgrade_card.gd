74e72689d9e6ef8a81e5e27b98cba180995190ce
Fernando Freitas <freitax.li@gmail.com>
2024-04-14 20:10:19 -0300

    chore: Criação de funções de gui e compra de meta upgrade

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -1,29 +1,51 @@
 extends PanelContainer
 class_name MetaUpgradeCard
 
-@onready var label_nome: Label = %Nome
-@onready var label_descricao: Label = %Descricao
+@onready var nome: Label = %Nome
+@onready var descricao: Label = %Descricao
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var progress_bar: ProgressBar = %ProgressBar
+@onready var botao_comprar: Button = %BotaoComprar
+@onready var label_progresso: Label = %LabelProgresso
 
-func set_upgrade_abilidade(upgrade: UpgradeAbilidade) -> void:
-	label_nome.text = upgrade.nome
-	label_descricao.text = upgrade.descricao
+var upgrade_card: MetaUpgrade
+
+func set_meta_upgrade_abilidade(upgrade: MetaUpgrade) -> void:
+	upgrade_card = upgrade
+	
+	nome.text = upgrade.titulo
+	descricao.text = upgrade.descricao
+	
+	atualizar_progresso()
+
+
+func atualizar_progresso() -> void:
+	var financa_atual = ProgressaoMeta.dados_salvos["financas_meta_upgrade"]
+	
+	var porcentagem = financa_atual / upgrade_card.custo_experiencia
+	
+	porcentagem = min(porcentagem, 1)
+	
+	progress_bar.value = porcentagem
+	
+	botao_comprar.disabled = porcentagem < 1
+	
+	label_progresso.text = str(financa_atual) + "/" + str(upgrade_card.custo_experiencia)
 
 
 func selecionar_card() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-		
 	animation_player.play("selecionado")
 	
 
-func _on_mouse_entered() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-
-
-func _on_mouse_exited() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-
-
-func _on_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
-		selecionar_card()
+func _on_botao_comprar_pressed() -> void:
+	if upgrade_card == null:
+		return
+	
+	ProgressaoMeta.adicionar_upgrade_meta(upgrade_card)
+	ProgressaoMeta.dados_salvos["financas_meta_upgrade"] -= upgrade_card.custo_experiencia
+	ProgressaoMeta.adicionar_upgrade_meta(upgrade_card)
+	
+	get_tree().call_group("meta_card_upgrade", "atualizar_progresso")
+	
+	animation_player.play("selecionado")
+	
