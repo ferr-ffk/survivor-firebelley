dec4fc851e7510b7ebcc8f8ef2ca9eb446a30bd4
Fernando Freitas <freitax.li@gmail.com>
2024-04-14 19:23:41 -0300

    chore: Adição de varíavel de custo para meta upgrades

--- resources/progressoes_meta/meta_upgrade.gd
+++ resources/progressoes_meta/meta_upgrade.gd
@@ -3,5 +3,6 @@ class_name MetaUpgrade
 
 @export var id: String
 @export var quantidade_maxima = 1
+@export var custo_experiencia: int = 10
 @export var titulo: String
 @export_multiline var descricao: String
