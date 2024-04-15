68ae0fb77acec5d9283ff17a8d695548aecfacd0
Fernando Freitas <freitax.li@gmail.com>
2024-04-14 21:09:18 -0300

    chore: Criar funcionalidade de quantidade máxima e porcentagem excedida

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -7,8 +7,10 @@ class_name MetaUpgradeCard
 @onready var progress_bar: ProgressBar = %ProgressBar
 @onready var botao_comprar: Button = %BotaoComprar
 @onready var label_progresso: Label = %LabelProgresso
+@onready var label_conta: Label = %LabelConta
 
 var upgrade_card: MetaUpgrade
+
 
 func set_meta_upgrade_abilidade(upgrade: MetaUpgrade) -> void:
 	upgrade_card = upgrade
@@ -20,17 +22,29 @@ func set_meta_upgrade_abilidade(upgrade: MetaUpgrade) -> void:
 
 
 func atualizar_progresso() -> void:
+	var quantidade_atual = 0
+	
+	# o jogo da crash se o upgrade não existe e é referenciado um campo dele
+	if ProgressaoMeta.dados_salvos["upgrades_meta"].has(upgrade_card.id):
+		quantidade_atual = ProgressaoMeta.dados_salvos["upgrades_meta"][upgrade_card.id]["quantidade"]
+	
 	var financa_atual = ProgressaoMeta.dados_salvos["financas_meta_upgrade"]
 	
 	var porcentagem = financa_atual / upgrade_card.custo_experiencia
 	
 	porcentagem = min(porcentagem, 1)
-	
 	progress_bar.value = porcentagem
-	
-	botao_comprar.disabled = porcentagem < 1
-	
 	label_progresso.text = str(financa_atual) + "/" + str(upgrade_card.custo_experiencia)
+	
+	var experiencia_insuficiente = porcentagem < 1
+	var quantidade_maxima_atingida = quantidade_atual >= upgrade_card.quantidade_maxima
+	
+	botao_comprar.disabled = experiencia_insuficiente || quantidade_maxima_atingida
+	
+	if quantidade_maxima_atingida:
+		botao_comprar.text = "Maximizado"
+	
+	label_conta.text = "x%d" % quantidade_atual
 
 
 func selecionar_card() -> void:
@@ -43,9 +57,9 @@ func _on_botao_comprar_pressed() -> void:
 	
 	ProgressaoMeta.adicionar_upgrade_meta(upgrade_card)
 	ProgressaoMeta.dados_salvos["financas_meta_upgrade"] -= upgrade_card.custo_experiencia
-	ProgressaoMeta.adicionar_upgrade_meta(upgrade_card)
+	ProgressaoMeta.salvar()
 	
 	get_tree().call_group("meta_card_upgrade", "atualizar_progresso")
 	
-	animation_player.play("selecionado")
+	selecionar_card()
 	
