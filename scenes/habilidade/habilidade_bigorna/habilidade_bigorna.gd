f7b7bb6730cb57e99767ea094653a6e0690c48c4
Fernando Freitas <freitax.li@gmail.com>
2024-04-15 21:43:57 -0300

    chore: Mudança de lógica de spawn de bigorna

--- scenes/habilidade/habilidade_bigorna/habilidade_bigorna.gd
+++ scenes/habilidade/habilidade_bigorna/habilidade_bigorna.gd
@@ -2,9 +2,3 @@ extends Node2D
 class_name HabilidadeBigorna
 
 @onready var componente_hit_box: ComponenteHitBox = $ComponenteHitBox
-
-func _ready() -> void:
-	EventosJogo.habilidade_adicionada.connect(_on_habilidade_adicionada)
-	
-func _on_habilidade_adicionada(upgrade: UpgradeHabilidade) -> void:
-	pass
--- scenes/habilidade/habilidade_bigorna_controller/habilidade_bigorna_controller.gd
+++ scenes/habilidade/habilidade_bigorna_controller/habilidade_bigorna_controller.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name HabilidadeBigornaController
 
-@export var alcance_maximo: int = 150
+@export var alcance_maximo: int = 70
 @export var dano_base: float = 40.0
 @export var intervalo_ataque: float = 5
 
@@ -9,34 +9,39 @@ class_name HabilidadeBigornaController
 
 var habilidade_bigorna_cena = preload("res://scenes/habilidade/habilidade_bigorna/habilidade_bigorna.tscn")
 
+var contagem_bigornas: int = 0
+
 func _ready() -> void:
 	$Timer.wait_time = intervalo_ataque
 
+	EventosJogo.habilidade_adicionada.connect(_on_habilidade_adicionada)
+	
+	
+func _on_habilidade_adicionada(upgrades_atuais: Dictionary, upgrade: UpgradeHabilidade) -> void:
+	if upgrade.id == "quantidade_bigorna":
+		contagem_bigornas = upgrades_atuais["quantidade_bigorna"]["quantidade"]
+
+
 func _on_timer_timeout() -> void:
-	var inimigos: Array = get_tree().get_nodes_in_group("enemies")
-	var player: Node2D = get_tree().get_first_node_in_group("player")
+	var inimigos: Array = get_tree().get_nodes_in_group("enemies") as Array
+	var player: Node2D = get_tree().get_first_node_in_group("player") as Node2D
 
 	if player == null:
 		push_error("Nenhum jogador no grupo player!")
 	
-	# filtra todos os inimigos que sua distância ao quadrado até o player é maior que o alcance máximo ao quadrado
-	inimigos = inimigos.filter(func(inimigo: Node2D): 
-		return inimigo.global_position.distance_squared_to(player.global_position) < pow(alcance_maximo, 2)
-	)
+	var direcao = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	
-	if inimigos.size() == 0:
-		return
+	var posicao_spawn = player.global_position + (direcao * randf_range(0, alcance_maximo))
 	
-	# ordena os inimigos em ordem crescente de distancia
-	inimigos.sort_custom(func(a: Node2D, b: Node2D):
-		var a_distancia = a.global_position.distance_squared_to(player.global_position)
-		var b_distancia = b.global_position.distance_squared_to(player.global_position)
+	var parametro_query = PhysicsRayQueryParameters2D.create(player.global_position, posicao_spawn)
+	
+	var resultado = get_tree().root.world_2d.direct_space_state.intersect_ray(parametro_query)
+	
+	if !resultado.is_empty():
+		posicao_spawn = resultado["position"]
 		
-		return a_distancia < b_distancia
-	)
+	var bigorna = habilidade_bigorna_cena.instantiate() as HabilidadeBigorna
 	
-	var bigorna: HabilidadeBigorna = habilidade_bigorna_cena.instantiate()
-	camada_primeiro_plano.add_child(bigorna)
+	get_tree().get_first_node_in_group("primeiro_plano").add_child(bigorna)
+	bigorna.global_position = posicao_spawn
 	bigorna.componente_hit_box.dano = dano_base
-	
-	bigorna.global_position = inimigos[0].global_position
